9b2aed429a83df2e7710f9bda3a1b996671b9726
Kobi Follan <kobifollan@gmail.com>
2022-05-25 13:18:04 +1000

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,23 +7,275 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "Crypto4Bies";
+const description = "5000 Unique Hand Drawn 4WDs";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 1000,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "40 Background" },
+      { name: "40 Livery" },
+      { name: "40 Bullbars" },
+      { name: "40 Outline" },
+      { name: "40 Rearbar" },
+      { name: "40 Roof" },
+      { name: "40 Jack"},
+      { name: "40 Wheels" },
     ],
+  },{
+    growEditionSizeTo: 2000,
+    layersOrder: [
+      { name: "60 Background" },
+      { name: "60 Livery" },
+      { name: "60 Outline" },
+      { name: "60 Bullbar" },
+      { name: "60 Rearbar" },
+      { name: "60 Roof" },
+      { name: "60 Decal" },
+      { name: "60 Wheels"},
+    ],
+  },{
+    growEditionSizeTo: 3000,
+    layersOrder: [
+      { name: "GU Backgrounds" },
+      { name: "GU Snorkel"},
+      { name: "GU Colour" },
+      { name: "GU Body" },
+      { name: "GU Decal" },
+      { name: "GU RearBar" },
+      { name: "GU Roof" },
+      { name: "GU BullBar" },
+      { name: "GU Wheels" },
+    ],
+  },{
+    growEditionSizeTo: 4000,
+    layersOrder: [
+      { name: "Nav Background" },
+      { name: "Nav Body" },
+      { name: "Nav Snorkel" },
+      { name: "Nav Roof Racks" },
+      { name: "Nav BullBar" },
+      { name: "Nav Decals"},
+      { name: "Nav wind deflectors"},
+      { name: "Nav Rear" },
+      { name: "Nav Wheels"},
+    ],
+  },{
+    growEditionSizeTo: 4900,
+    layersOrder: [
+      { name: "Zook Background" },
+      { name: "Zook Colour" },
+      { name: "Zook Outline" },
+      { name: "Zook Bullbar" },
+      { name: "Zook RearBar" },
+      { name: "Zook Wind Deflectors"},
+      { name: "Zook Decals"},
+      { name: "Zook Roof" },
+      { name: "Zook Wheels"},
+    ],
+  },{
+    growEditionSizeTo: 4901,
+layersOrder: [
+  { name: "grubby Edition 1"}
+]
+  },{
+    growEditionSizeTo: 4902,
+layersOrder: [
+  { name: "grubby Edition 2"}
+]
+  },{
+    growEditionSizeTo: 4903,
+layersOrder: [
+  { name: "grubby Edition 3"}
+]
+  },{
+    growEditionSizeTo: 4904,
+layersOrder: [
+  { name: "grubby Edition 4"}
+]
+  },{
+    growEditionSizeTo: 4905,
+layersOrder: [
+  { name: "grubby Edition 5"}
+]
+  },{
+    growEditionSizeTo: 4906,
+layersOrder: [
+  { name: "grubby Edition 6"}
+]
+  },{
+    growEditionSizeTo: 4907,
+layersOrder: [
+  { name: "grubby Edition 7"}
+]
+  },{
+    growEditionSizeTo: 4908,
+layersOrder: [
+  { name: "grubby Edition 8"}
+]
+  },{
+    growEditionSizeTo: 4909,
+layersOrder: [
+  { name: "grubby Edition 9"}
+]
+  },{
+    growEditionSizeTo: 4910,
+layersOrder: [
+  { name: "grubby Edition 10"}
+]
+  },{
+    growEditionSizeTo: 4911,
+layersOrder: [
+  { name: "Kieran Edition 1"}
+]
+  },{
+    growEditionSizeTo: 4912,
+layersOrder: [
+  { name: "Kieran Edition 2"}
+]
+  },{
+    growEditionSizeTo: 4913,
+layersOrder: [
+  { name: "Kieran Edition 3"}
+]
+  },{
+    growEditionSizeTo: 4914,
+layersOrder: [
+  { name: "Kieran Edition 4"}
+]
+  },{
+    growEditionSizeTo: 4915,
+layersOrder: [
+  { name: "Kieran Edition 5"}
+]
+  },{
+    growEditionSizeTo: 4916,
+layersOrder: [
+  { name: "Kieran Edition 6"}
+]
+  },{
+    growEditionSizeTo: 4917,
+layersOrder: [
+  { name: "Kieran Edition 7"}
+]
+  },{
+    growEditionSizeTo: 4918,
+layersOrder: [
+  { name: "Kieran Edition 8"}
+]
+  },{
+    growEditionSizeTo: 4919,
+layersOrder: [
+  { name: "Kieran Edition 9"}
+]
+  },{
+    growEditionSizeTo: 4920,
+layersOrder: [
+  { name: "Kieran Edition 10"}
+]
+  },{
+    growEditionSizeTo: 4921,
+layersOrder: [
+  { name: "LR Edition 1"}
+]
+  },{
+    growEditionSizeTo: 4922,
+layersOrder: [
+  { name: "LR Edition 2"}
+]
+  },{
+    growEditionSizeTo: 4923,
+layersOrder: [
+  { name: "LR Edition 3"}
+]
+  },{
+    growEditionSizeTo: 4924,
+layersOrder: [
+  { name: "LR Edition 4"}
+]
+  },{
+    growEditionSizeTo: 4925,
+layersOrder: [
+  { name: "LR Edition 5"}
+]
+  },{
+    growEditionSizeTo: 4926,
+layersOrder: [
+  { name: "LR Edition 6"}
+]
+  },{
+    growEditionSizeTo: 4927,
+layersOrder: [
+  { name: "LR Edition 7"}
+]
+  },{
+    growEditionSizeTo: 4928,
+layersOrder: [
+  { name: "LR Edition 8"}
+]
+  },{
+    growEditionSizeTo: 4929,
+layersOrder: [
+  { name: "LR Edition 9"}
+]
+  },{
+    growEditionSizeTo: 4930,
+layersOrder: [
+  { name: "LR Edition 10"}
+]
+  },{
+    growEditionSizeTo: 4931,
+layersOrder: [
+  { name: "PT Edition 1"}
+]
+  },{
+    growEditionSizeTo: 4932,
+layersOrder: [
+  { name: "PT Edition 2"}
+]
+  },{
+    growEditionSizeTo: 4933,
+layersOrder: [
+  { name: "PT Edition 3"}
+]
+  },{
+    growEditionSizeTo: 4934,
+layersOrder: [
+  { name: "PT Edition 4"}
+]
+  },{
+    growEditionSizeTo: 4935,
+layersOrder: [
+  { name: "PT Edition 5"}
+]
+  },{
+    growEditionSizeTo: 4936,
+layersOrder: [
+  { name: "PT Edition 6"}
+]
+  },{
+    growEditionSizeTo: 4937,
+layersOrder: [
+  { name: "PT Edition 7"}
+]
+  },{
+    growEditionSizeTo: 4938,
+layersOrder: [
+  { name: "PT Edition 8"}
+]
+  },{
+    growEditionSizeTo: 4939,
+layersOrder: [
+  { name: "PT Edition 9"}
+]
+  },{
+    growEditionSizeTo: 4940,
+layersOrder: [
+  { name: "PT Edition 10"}
+]
   },
 ];
 
@@ -32,13 +284,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 1080,
+  height: 1080,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://www.crypto4bies.au", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -49,22 +301,22 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'Crypto4Bies';
+const CONTRACT_SYMBOL = 'C4B';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0xDE525C7BF5B0921B920b060Bfc074689856ef2cE';
+const TREASURY_ADDRESS = '0xDE525C7BF5B0921B920b060Bfc074689856ef2cE';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-05-27T22:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0xDE525C7BF5B0921B920b060Bfc074689856ef2cE"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
@@ -73,10 +325,10 @@ const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "Which Crypto4Bies Will You Get?"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeiazfuactf3kxmtth34qb5l2ky7ltckcfcpes6anf7ij36quvhd54e"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -106,7 +358,7 @@ const solanaMetadata = {
 
 const gif = {
   export: false,
-  repeat: 0,
+  repeat: 10,
   quality: 100,
   delay: 500,
 };
@@ -137,7 +389,7 @@ const background = {
 
 const rarityDelimiter = "#";
 
-const uniqueDnaTorrance = 10000;
+const uniqueDnaTorrance = Infinity
 
 const preview = {
   thumbPerRow: 5,
